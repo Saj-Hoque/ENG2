d80bb580dde99ce8a17651d4c1003211706f22d9
Saj-Hoque <sajuss01@gmail.com>
2025-05-15 20:10:01 +0100

    Recreate concrete syntax

--- language/uk.ac.york.cs.eng2.offers/src/uk/ac/york/cs/eng2/offers/impl/PercentageDiscountOnTaggedQuantityActionImpl.java
+++ language/uk.ac.york.cs.eng2.offers/src/uk/ac/york/cs/eng2/offers/impl/PercentageDiscountOnTaggedQuantityActionImpl.java
@@ -3,8 +3,8 @@
 package uk.ac.york.cs.eng2.offers.impl;
 
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.notify.NotificationChain;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 
